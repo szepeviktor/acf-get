005622c6e884b1d4ce0a362b50636a91b3011451
Viktor Szépe <viktor@szepe.net>
2019-09-05 11:06:36 +0000

    Relation fields return an empty string, not an empty array

--- inc/class-acfget.php
+++ inc/class-acfget.php
@@ -87,6 +87,10 @@ class ACFget {
 	 */
 	public static function array_field( string $selector, array $default = [] ) : array {
 		$raw_value = \get_field( $selector, static::$post_id );
+		// FIXME ACF shim.
+		if ( '' === $raw_value ) {
+			$raw_value = [];
+		}
 		if ( is_array( $raw_value ) ) {
 			return $raw_value;
 		}
