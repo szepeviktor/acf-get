730836d448721b947385206556f25aaef096c492
Viktor Szépe <viktor@szepe.net>
2019-11-12 12:40:38 +0000

    Add manual test runner script and a new keyword

---
+++ tests/circle-ci.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Get all jobs but the first
+for R in $(seq 2 $(shyaml get-length 'jobs.test.steps' <.circleci/config.yml)); do
+    shyaml get-value "jobs.test.steps.$((R-1)).run" <.circleci/config.yml
+    echo
+#      Execute job
+done | xargs -I% bash -x -c "%"
