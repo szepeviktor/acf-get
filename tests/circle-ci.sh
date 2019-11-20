55716fa1c1562bfd2b82355387f0b79c219f31cc
Viktor Szépe <viktor@szepe.net>
2019-11-20 11:03:58 +0000

    Improve manual CircleCi runner

--- tests/circle-ci.sh
+++ tests/circle-ci.sh
@@ -2,7 +2,7 @@
 
 # Get all jobs but the first
 for R in $(seq 2 $(shyaml get-length 'jobs.test.steps' <.circleci/config.yml)); do
-    shyaml get-value "jobs.test.steps.$((R-1)).run" <.circleci/config.yml
+    shyaml get-value "jobs.test.steps.$((R - 1)).run" <.circleci/config.yml
     echo
 #      Execute job
-done | xargs -I% bash -x -c "%"
+done | xargs -I "%" bash -x -c "%"
